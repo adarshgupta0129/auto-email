6880275ccbe70c3769201cb9c973c32c87109f12
adarsh.gupta <adarsh.gupta@ultragenicglobal.com>
2025-12-16 18:09:02 +0530

    Auto Email bug fixes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8000;
 const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.LOCAL_GOOGLE_REDIRECT_URI
+    process.env.GOOGLE_REDIRECT_URI
 );
 
 // Scopes for Gmail access
